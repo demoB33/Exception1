ba63452b82614c09dda4306954d83161c9d9cbb2
demoB33 <vasilenko-s@list.ru>
2023-03-26 14:58:54 +0700

    Final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean input = Validator.validate("12343DFDFfd","_sldsdl1!","_sldsdl");
+        if (input) {
+            System.out.println("Регистрация прошла успешно!");
+        } else {
+            System.out.println("Не правильно введен логин и/или пароль");
+        }
+
     }
 }
